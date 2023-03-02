22f7c2367faab0534948050b86ec1b30ba3f3168
Mohammed <m97.chahboun@gmail.com>
2023-03-02 15:43:27 +0100

    Minor fixes

--- traduction/trad/forms.py
+++ traduction/trad/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class trd(forms.Form):
-    style = forms.TextInput(attrs={'class':"form-control", 'placeholder':"Enter todo e.g. Delete junk files", 'aria-label':"Todo", 'aria-describedby':"add-btn"})
+    style = forms.TextInput(attrs={'class':"form-control", 'placeholder':"Enter Word", 'aria-label':"Traduction", 'aria-describedby':"add-btn"})
     word = forms.CharField(max_length=120,widget=style)
